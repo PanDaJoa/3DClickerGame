b8a73214918a55b1cee04a845d142cd6c48c1bfb
PanDaJoa <jms3590743@gmail.com>
2024-04-17 11:21:18 +0900

    게이지가 0.95 이상 일 때 클릭시 2배 구현

--- Assets/02.Scripts/FeverGauge.cs
+++ Assets/02.Scripts/FeverGauge.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro.EditorUtilities;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,7 +11,9 @@ public class FeverGauge : MonoBehaviour
     public float decreaseRate = 0.05f; // 게이지가 감소하는 속도
     private float GaugeValue  = 0f;   // 현재 게이지 값
     public float maxGaugeValue = 1.0f; // 게이지 최대 값
-
+   
+    bool isHitPowerDoubled = false; // 게이지 두배 확인
+    private bool hasClicked = false; // 마우스 클릭 확인
     public Slider FeverUI;
 
     public Frog _frog;
@@ -22,28 +25,34 @@ public class FeverGauge : MonoBehaviour
     void Update()
     {
         FeverUI.value = GaugeValue / maxGaugeValue;
-        if (Input.GetMouseButton(0)) 
+        if (Input.GetMouseButton(0))
         {
            IncreaseGauge();
-
         }
         else
         {
-            
             DecreaseGauge();
         }
     }
 
-    public void FrogClicked()
-    {
-        IncreaseGauge();
-    }
-
     void IncreaseGauge() // 증가
     {
+        if (GaugeValue < 0.95f && GaugeValue + increaseRate * Time.deltaTime >= 0.95f)
+        {
+            _frog.HitPower *= 2f;
+            isHitPowerDoubled = true; 
+        }
+
+        // 게이지 증가
         GaugeValue += increaseRate * Time.deltaTime;
         GaugeValue = Mathf.Clamp(GaugeValue, 0.0f, maxGaugeValue);
         Debug.Log("게이지 증가: " + GaugeValue);
+
+        if (isHitPowerDoubled && GaugeValue < 0.95f)
+        {
+            _frog.HitPower /= 2f;
+            isHitPowerDoubled = false;
+        }
     }
 
     void DecreaseGauge() // 감소
